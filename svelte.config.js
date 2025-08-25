fbcebe0374fa9d215fbac102ade3273eb9a97d5f
Flourishtalents <150746845+Flourishtalents@users.noreply.github.com>
2025-08-25 16:02:17 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,30 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
 	kit: {
-		// Configure for SPA mode
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
-		
-		// Ensure all routes are prerendered for SPA
-		prerender: {
-			entries: ['*'],
-			handleHttpError: 'warn',
-			handleMissingId: 'warn'
-		}
+		adapter: adapter()
 	}
 };
 
